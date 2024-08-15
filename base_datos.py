64ae0da0c4bef5315782d3019a85652e5d187679
PatricioVargasR <1722110158@utectulancingo.edu.mx>
2024-08-15 15:59:05 -0600

    Cambios

--- base_datos.py
+++ base_datos.py
@@ -6,13 +6,21 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+
+# Credenciales
+TURSO_DATABASE_URL = os.environ['TURSO_URL']
+TURSO_AUTH_TOKEN = os.environ['TURSO_AUTH']
+
 
 # Definir URL de la base de datos
-SQLALCHEMY_DATABASE_URL = "sqlite:///./inmobiliaria.db"
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./inmobiliaria.db"
+dbUrl = f"sqlite+{TURSO_DATABASE_URL}/?authToken={TURSO_AUTH_TOKEN}&secure=true"
+
 
 # Crear el motor de conexión a la base de datos utilizando la URL
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    dbUrl, connect_args={"check_same_thread": False}
 )
 
 # Crear la sesión de conexión a la base de datos
